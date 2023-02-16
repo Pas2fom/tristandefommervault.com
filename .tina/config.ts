ffac7a5d4c919b3667391b24aa1be7e77df55bde
Tristan <36541230+Pas2fom@users.noreply.github.com>
2023-02-16 13:15:15 +0100

    Update config.ts
    retour sans js

--- .tina/config.ts
+++ .tina/config.ts
@@ -25,15 +25,7 @@ export default defineConfig({
         path: "content/formations",
         fields: [
           {
-            {
-          name: "json_field",
-          label: "JSON Field",
-          component: "json",
-        },
-      ],
-  },
-],
-},
+
             type: "rich-text",
             name: "body",
             label: "Body of Document",
